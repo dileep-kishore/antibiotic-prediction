66ad58787b3040f32dd15ad5459868ea70449039
Dileep Kishore <k.dileep1994@gmail.com>
2023-11-15 08:55:59 -0500

    Fix: Move from docker compose back to single container

--- Dockerfile
+++ Dockerfile
@@ -4,36 +4,39 @@ FROM mambaorg/micromamba
 LABEL software="antibiotic-prediction"
 LABEL version="0.1.0"
 
-MAINTAINER name="Dileep Kishore" email="kishored@ornl.gov"
+LABEL authors.name="Dileep Kishore"
+LABEL authors.email="kishored@ornl.gov"
 
-USER 0
+# Run as root
+USER root
+
 RUN apt-get update && apt-get -y upgrade \
   && apt-get install -y --no-install-recommends \
   git \
   wget \
   g++ \
   ca-certificates \
+  apt-transport-https \
+  gnupg \
   && rm -rf /var/lib/apt/lists/*
 
+# Run as mamba user
+USER $MAMBA_USER
+
 # mamba setup
-RUN micromamba shell init --shell=bash --prefix=~/micromamba
+RUN micromamba shell init --shell=bash --prefix=/micromamba
 
 # Copy setup scripts
 COPY setup/install_antismash.sh /deps/
-COPY setup/install_rgi.sh /deps/
-COPY setup/install_natural_product.sh /deps/
-
-# Copy env
+# Copy env files
 COPY setup/env_rgi5.yml /deps/
 COPY setup/env_natural_product.yml /deps/
 
-# Run setup scripts
-# TODO: Replace antismash and rgi with public images?
-# RUN bash /deps/install_antismash.sh
-# RUN bash /deps/install_rgi.sh $INS_COMMIT
-# RUN bash /deps/install_natural_product.sh $INS_COMMIT
+# Set up the environments
+RUN bash /deps/install_antismash.sh
 RUN micromamba env create -f /deps/env_natural_product.yml
-
+RUN micromamba env create -f /deps/env_rgi5.yml
+RUN micromamba clean --all --yes
 
 # Set up antibiotic-prediction repo
 WORKDIR /antibiotic-prediction
--- setup/install_antismash.sh
+++ setup/install_antismash.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
 
 mkdir -p /tmp
-cd /tmp
+cd /tmp || exit
 
-apt-get update && \
-    apt-get -y install apt-transport-https gnupg
+# Install deps via Debian/Ubuntu (https://docs.antismash.secondarymetabolites.org/install/)
+# wget http://dl.secondarymetabolites.org/antismash-stretch.list -O /etc/apt/sources.list.d/antismash.list &&
+#     wget -q -O- http://dl.secondarymetabolites.org/antismash.asc | apt-key add -
+# apt-get update &&
+#     apt-get -y install hmmer2 hmmer diamond-aligner fasttree prodigal ncbi-blast+ muscle glimmerhmm
 
-wget http://dl.secondarymetabolites.org/antismash-stretch.list -O /etc/apt/sources.list.d/antismash.list && \
-    wget -q -O- http://dl.secondarymetabolites.org/antismash.asc | apt-key add -
+# export PATH="/micromambaforge/bin:$PATH"
+micromamba create -n antismash python=3.9
+micromamba activate antismash
 
-apt-get update && \
-    apt-get -y install hmmer2 hmmer diamond-aligner fasttree prodigal ncbi-blast+ muscle glimmerhmm
+# Install deps via conda (https://docs.antismash.secondarymetabolites.org/install/)
+micromamba install -c bioconda hmmer2 hmmer diamond=2.0.14 fasttree prodigal blast muscle glimmerhmm
 
-export PATH="/mambaforge/bin:$PATH"
+wget https://dl.secondarymetabolites.org/releases/7.0.1/antismash-7.0.1.tar.gz &&
+    tar -zxf antismash-7.0.0.tar.gz
 
-mamba create -n antismash python=3.9
-source activate antismash
-
-# Alternative to line 12
-# mamba install -c bioconda hmmer2 hmmer diamond=2.0.14 fasttree prodigal blast muscle glimmerhmm
-
-wget https://dl.secondarymetabolites.org/releases/7.0.0/antismash-7.0.0.tar.gz  \
-      && tar -zxf antismash-7.0.0.tar.gz
-
-pip install ./antismash-7.0.0
-download-antismash-databases && antismash --check-prereqs
-
-# pip3 install nose jinja2
-# pip3 install jsonrpcbase numpy pandas biopython
-# pip3 install coverage
+pip install ./antismash-7.0.1
+download-antismash-databases && antismash --prepare-data
